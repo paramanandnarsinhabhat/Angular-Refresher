05c32dde24f589a2ac7d04f79720577dc06cfdd0
paramanandnarsinhabhat <141020967+paramanandnarsinhabhat@users.noreply.github.com>
2024-04-02 11:45:03 +0530

    feat(product-model): define Product class
    Created a new Product class with properties for id, name, description, price, and availability. This class will be used to create product instances throughout the application, ensuring consistent data structure and type safety.

--- angular-refresher/src/app/models/product.model.ts
+++ angular-refresher/src/app/models/product.model.ts
@@ -1,2 +1,12 @@
 export class Product {
+    /**
+     *
+     */
+    constructor(
+        public id: number,
+        public name : string,
+        public description : string,
+        public price : number,
+        public available : boolean
+    ) {}
 }
